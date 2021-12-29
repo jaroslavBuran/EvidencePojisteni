be892b63e1e93596a4ad43b1f5441c8520a98df7
Jaroslav Buráň <jaroslav.buran95@gmail.com>
2021-12-29 12:06:04 +0100

    úprava FE

--- EvidencePojisteni/Models/RegisterViewModel.cs
+++ EvidencePojisteni/Models/RegisterViewModel.cs
@@ -19,10 +19,15 @@ namespace EvidencePojisteni.Models
         [Display(Name = "Potvrzení hesla")]
         [Compare("Password", ErrorMessage = "Zadaná hesla se musí shodovat.")]
         public string ConfirmPassword { get; set; }
-
+        [Required]
+        [Display(Name = "Jméno")]
         public string FirstName { get; set; }
-
+        [Required]
+        [Display(Name = "Příjmení")]
         public string LastName { get; set; }
+        [Required]
+        [Display(Name = " Rodné číslo (bez lomítka)")]
+        //[RegularExpression] doplnit, aby byl splněn formát jen čísla bez lomítka
         public string IdNumber { get; set; }
     }
 }
