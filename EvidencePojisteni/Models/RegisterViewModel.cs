d612ebe052b515df750350f4e97e629a35ce3c36
Jaroslav Buráň <jaroslav.buran95@gmail.com>
2021-12-29 14:18:20 +0100

    úpravy FE (responzivita)

--- EvidencePojisteni/Models/RegisterViewModel.cs
+++ EvidencePojisteni/Models/RegisterViewModel.cs
@@ -4,30 +4,35 @@ namespace EvidencePojisteni.Models
 {
     public class RegisterViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Email musí být vyplněn!")]
         [EmailAddress(ErrorMessage = "Neplatná emailová adresa")]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Heslo musí být vyplněno!")]  //dořešit ještě dostatečnou sílu hesla
         [StringLength(100, ErrorMessage = "{0} musí mít délku alespoň {2} a nejvíc {1} znaků.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Heslo")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Potvrzení hesla musí být vyplněno!")]
         [DataType(DataType.Password)]
         [Display(Name = "Potvrzení hesla")]
         [Compare("Password", ErrorMessage = "Zadaná hesla se musí shodovat.")]
         public string ConfirmPassword { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Jméno musí být vyplněno!")]
         [Display(Name = "Jméno")]
         public string FirstName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Příjmení musí být vyplněno!")]
         [Display(Name = "Příjmení")]
         public string LastName { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Rodné číslo musí být vyplněno!")]
         [Display(Name = " Rodné číslo (bez lomítka)")]
-        //[RegularExpression] doplnit, aby byl splněn formát jen čísla bez lomítka
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Rodné číslo musí obsahovat pouze číslice!")]
+        [StringLength(10, ErrorMessage = "Rodné číslo musí obsahovat 9, případně 10 číslic!", MinimumLength = 9)]
         public string IdNumber { get; set; }
     }
 }
