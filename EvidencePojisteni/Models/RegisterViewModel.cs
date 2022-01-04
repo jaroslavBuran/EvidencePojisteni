337a8440975478215289bd651f56a02ea4143588
Jaroslav Buráň <jaroslav.buran95@gmail.com>
2022-01-04 17:54:35 +0100

    implementace rozhraní pro změnu hesla, rozpracována správa rolí

--- EvidencePojisteni/Models/RegisterViewModel.cs
+++ EvidencePojisteni/Models/RegisterViewModel.cs
@@ -11,14 +11,15 @@ namespace EvidencePojisteni.Models
 
         [Required(ErrorMessage = "Heslo musí být vyplněno!")]  //dořešit ještě dostatečnou sílu hesla
         [StringLength(100, ErrorMessage = "{0} musí mít délku alespoň {2} a nejvíc {1} znaků.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
+        [RegularExpression("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{5,}$", ErrorMessage ="Heslo musí obsahovat alespoň jeden speciální znak, alespoň jedno číslo a alespoň jedno velké a jedno malé písmeno")]
+        [DataType(DataType.Password)] //heslo musí obsahovat aspoň jedno číslo
         [Display(Name = "Heslo")]
         public string Password { get; set; }
 
         [Required(ErrorMessage = "Potvrzení hesla musí být vyplněno!")]
         [DataType(DataType.Password)]
         [Display(Name = "Potvrzení hesla")]
-        [Compare("Password", ErrorMessage = "Zadaná hesla se musí shodovat.")]
+        [Compare("Password", ErrorMessage = "Zadaná hesla se musí shodovat!")]
         public string ConfirmPassword { get; set; }
 
         [Required(ErrorMessage = "Jméno musí být vyplněno!")]
